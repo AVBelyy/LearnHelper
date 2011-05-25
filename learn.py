ce6eb2f4f80025dbfee729c8e0091628ce8affc4
Anton Belyy <anton.belyy@gmail.com>
2011-05-25 20:48:56 +0400

    second commit

--- learn.py
+++ learn.py
@@ -18,12 +18,23 @@ db = connection.cursor()
 lang_id = 1
 repeat = []
 correct = True
+start = time()
+
+if "--reset" in sys.argv[1:]:
+	db.execute("UPDATE words SET last_repeat=0 WHERE lang_id=?", (lang_id,))
+	db.close()
+	connection.commit()
+	exit(0)
 
 repeat_time = db.execute("SELECT repeat_time FROM dictionaries WHERE id=?", (lang_id,)).fetchone()[0]
 rows = db.execute("SELECT * FROM words WHERE lang_id=?", (lang_id,))
+all, repeated = 0, 0
 for row in rows:
+	all += 1
 	if row[4] + repeat_time < int(time()):
 		repeat.append(row)
+	else:
+		repeated += 1
 
 while True:
 	try:
@@ -31,10 +42,23 @@ while True:
 			try:
 				word = choice(repeat)
 			except IndexError: # repeat is empty
-				print "All done!"
+				cur = time()
+				delta = int(cur-start)
+				if cur - start < 1.0:
+					print u"Приходите позже!"
+				else:
+					print u"Повторено за %s" % delta,
+					if   (delta % 100) not in (11, 12, 13, 14) and \
+					     (delta % 1000) not in (11, 12, 13, 14):
+						if (delta % 10) in (2, 3, 4): print u"секунды"
+						elif (delta % 10) == 1:       print u"секунду"
+						else: print u"секунд"
+					else:
+						print u"секунд"
 				break
+			repeated += 1
 			del repeat[repeat.index(word)]
-		print word[2] + "?",
+		print ("[%s/%s] " % (repeated, all)) + word[2] + "?",
 		translation = raw_input().decode(sys.stdout.encoding).lower()
 	except EOFError:
 		break
@@ -43,5 +67,5 @@ while True:
 		db.execute("UPDATE words SET last_repeat=? WHERE id=?", (int(time()), word[0]))
 
 print
-connection.commit() # save changes
 db.close()
+connection.commit() # save changes
